73c0e85646a2256fd699a5cb077e8e49190e9ae4
MilanPrpa <milanprpa25@gmail.com>
2021-08-19 08:14:34 -0700

    Countdown to Croatia complete

--- countdown-timer/index.js
+++ countdown-timer/index.js
@@ -1,3 +1,7 @@
+const monthsEl = document.getElementById('months')
+const daysEl = document.getElementById('days')
+const hoursEl = document.getElementById('hours')
+const minutesEl = document.getElementById('minutes')
 const secondsEl = document.getElementById('seconds')
 
 const inCroatia = '1 May 2022';
@@ -8,15 +12,24 @@ function countDown() {
 
   const totalSeconds = (endDate - currentDate) / 1000;
   const seconds = Math.floor(totalSeconds) % 60;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const months = Math.floor(totalSeconds / 3600 / 24 * 0.032855);
 
+
+  monthsEl.innerHTML = months;
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = hours;
+  minutesEl.innerHTML = minutes;
   secondsEl.innerHTML = seconds;
 
-  console.log(seconds)
+
 }
 
 
 countDown()
 
-// setInterval(() => {
-//   countDown()
-// }, 1000)
+setInterval(() => {
+  countDown()
+}, 1000)
